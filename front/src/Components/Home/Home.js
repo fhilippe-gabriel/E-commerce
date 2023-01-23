d95c7b5e0196efe705d7f9d0daf3f26742e8c861
Fhilippe Gabriel <fhilippegabriel15@gmail.com>
2023-01-23 20:29:12 -0300

    :up:

--- front/src/Components/Home/Home.js
+++ front/src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import Header from "../Header/Header";
+
 const products = [
   {
     id: 1,
@@ -184,9 +185,81 @@ export default function Home() {
                 </div>
               </div>
             ))}
+            {products.map((product) => (
+              <div key={product.id} className="group relative">
+                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-green-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-100">
+                      <a href={product.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {product.name}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-green-200">{product.color}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-100">{product.price}</p>
+                </div>
+              </div>
+            ))}
+            {products.map((product) => (
+              <div key={product.id} className="group relative">
+                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-green-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-100">
+                      <a href={product.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {product.name}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-green-200">{product.color}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-100">{product.price}</p>
+                </div>
+              </div>
+            ))}
+            {products.map((product) => (
+              <div key={product.id} className="group relative">
+                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-green-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-100">
+                      <a href={product.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {product.name}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-green-200">{product.color}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-100">{product.price}</p>
+                </div>
+              </div>
+            ))}
           </div>
+
         </div>
+
       </div>
+
     </div>
   );
 }
