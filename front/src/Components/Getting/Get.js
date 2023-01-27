cb2602a019b79b06700f0356c8627cf99f1fc471
Fhilippe Gabriel <fhilippegabriel15@gmail.com>
2023-01-26 21:05:18 -0300

    :up:

--- front/src/Components/Getting/Get.js
+++ front/src/Components/Getting/Get.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from "../Header/Header";
+import BlockShop from "../BlockShop/BlockShop";
 import { StarIcon } from '@heroicons/react/20/solid'
 import { RadioGroup } from '@headlessui/react'
 const features = [
@@ -322,8 +323,8 @@ export default function Example() {
       <div className="bg-black">
         <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
           <div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
-            <p className="mt-4 text-gray-500">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl">Technical Specifications</h2>
+            <p className="mt-4 text-gray-200">
               The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated
               steel divider separates active cards from new ones, or can be used to archive important task lists.
             </p>
@@ -331,8 +332,8 @@ export default function Example() {
             <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
               {features.map((feature) => (
                 <div key={feature.name} className="border-t border-gray-200 pt-4">
-                  <dt className="font-medium text-gray-900">{feature.name}</dt>
-                  <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
+                  <dt className="font-medium text-gray-200">{feature.name}</dt>
+                  <dd className="mt-2 text-sm text-gray-300">{feature.description}</dd>
                 </div>
               ))}
             </dl>
@@ -361,6 +362,7 @@ export default function Example() {
           </div>
         </div>
       </div>
+      < BlockShop />
     </div>
   )
 }
